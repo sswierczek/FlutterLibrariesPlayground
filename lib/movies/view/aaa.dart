a72fa452c681606db826c9b5930d1a977ed43675
Sebastian Świerczek <sebastian.swierczek@gmail.com>
2018-11-21 21:38:27 +0100

    Add network client and images loading libraries

---
+++ lib/movies/view/aaa.dart
@@ -0,0 +1,4 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
--- lib/movies/view/movie_item.dart
+++ lib/movies/view/movie_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -20,6 +21,24 @@ class MoviesListItem extends StatelessWidget {
             color: Theme.of(context).accentColor,
             decoration: TextDecoration.none,
           )),
+      // TODO Load movie image
+      leading: _movieImageBox(new CachedNetworkImage(
+        imageUrl: "http://via.placeholder.com/350x200",
+        placeholder: new CircularProgressIndicator(),
+        errorWidget: new Icon(Icons.error),
+        fadeOutDuration: new Duration(seconds: 1),
+        fadeInDuration: new Duration(seconds: 3),
+      )),
+    );
+  }
+
+  Widget _movieImageBox(Widget child) {
+    return new SizedBox(
+      width: 100.0,
+      height: 100.0,
+      child: new Center(
+        child: child,
+      ),
     );
   }
 }
